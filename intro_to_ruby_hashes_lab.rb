b9475e96be11b0394129d4293117ac2cc5700b4b
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-24 18:58:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,11 +20,12 @@ def monopoly_with_second_tier
   	monopoly = {
 	 :railroads=>
 	 {
-	   :pieces=>4,
-	   :rent_in_dollars=>{},
-	 :names=>{}
+	   :pieces=>4
 	 }
-  	}
+	 :names=>{}
+	 :rent_in_dollars=>{},
+	 }
+  	
 end
 
 def monopoly_with_third_tier
